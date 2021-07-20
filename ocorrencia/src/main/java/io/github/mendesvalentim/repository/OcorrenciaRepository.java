2a97d34c8f47a9d14424122ad64280e95ced817b
Bruno <mendesvalentim@gmail.com>
2021-07-20 18:45:24 -0300

    ainda rodando

--- ocorrencia/src/main/java/io/github/mendesvalentim/repository/OcorrenciaRepository.java
+++ ocorrencia/src/main/java/io/github/mendesvalentim/repository/OcorrenciaRepository.java
@@ -1,7 +1,9 @@
 package io.github.mendesvalentim.repository;
 
 import io.github.mendesvalentim.model.Ocorrencia;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class OcorrenciaRepository {
     public void persistir(Ocorrencia ocorrencia) {
         //acessa a base e salva a ocorrencia
